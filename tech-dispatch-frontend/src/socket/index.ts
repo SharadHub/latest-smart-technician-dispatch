e62e3371318e2e461eb10467f22150f445a3f360
Sharad Bista <bishalbista737@gmail.com>
2026-05-01 23:27:40 +0545

    refactor(socket): remove stale type re-exports from socket barrel index

--- tech-dispatch-frontend/src/socket/index.ts
+++ tech-dispatch-frontend/src/socket/index.ts
@@ -3,8 +3,3 @@ export {
   disconnectSocket,
   subscribe,
 } from "./manager";
-export type {
-  SocketJoinPayload,
-  SocketBookingRequestPayload,
-  SocketBookingUpdatePayload,
-} from "../types";
